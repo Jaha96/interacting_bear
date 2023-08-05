8bfec9bea4e88c4749138f69a354781dfa126b64
Japar Jarkynbyek <jarkinbek73@gmail.com>
2023-08-05 21:42:38 +0900

    init project

---
+++ android/app/src/main/kotlin/com/example/interacting_tom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.interacting_tom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/presentation/animation_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+
+class AnimationScreen extends StatefulWidget {
+  const AnimationScreen({super.key});
+
+  @override
+  State<AnimationScreen> createState() => _AnimationScreenState();
+}
+
+class _AnimationScreenState extends State<AnimationScreen> {
+  Artboard? riveArtboard;
+  SMIBool? isHearing;
+  SMITrigger? wave;
+
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle.load('assets/bear_character.riv').then(
+      (data) async {
+        try {
+          final file = RiveFile.import(data);
+          final artboard = file.mainArtboard;
+          final controller =
+              StateMachineController.fromArtboard(artboard, 'State Machine 1');
+          if (controller != null) {
+            artboard.addController(controller);
+            isHearing = controller.findSMI('Hear');
+            wave = controller.findSMI('Wave');
+            setState(() => riveArtboard = artboard);
+          }
+        } catch (e) {
+          print(e);
+        }
+      },
+    );
+  }
+
+  void _toggleHearing() {
+    if (isHearing != null) {
+      isHearing!.value = !isHearing!.value;
+    } else {
+      isHearing!.value = true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: riveArtboard == null
+              ? const SizedBox()
+              : Rive(
+                  artboard: riveArtboard!,
+                  fit: BoxFit.cover,
+                )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleHearing,
+        tooltip: isHearing!.value ? 'Pause' : 'Play',
+        child: Icon(
+          isHearing!.value ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
